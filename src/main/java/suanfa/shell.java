27167ae0498d7973d6d6c5dd068ae63f862799f5
hongwu.yang <hongwu.yang@qunar.com>
2017-02-08 14:14:42 +0800

    希尔算法

--- src/main/java/suanfa/shell.java
+++ src/main/java/suanfa/shell.java
@@ -7,16 +7,17 @@ public class shell {
 
     public static void main(String[] args) {
         int [] num = {4,6,8,2,34,12,56,57,68,9};
-        int [] interval = {5,4,3,2,1};
+        int [] interval = {5,3,1};
         for (int d = 0; d < interval.length; d++) {
             int t = interval[d];
-            for (int i = 0; i + t < num.length; i += t) {
-                int temp;
-                if (num[i] > num[i + t]) {
-                    temp = num[i];
-                    num[i] = num[i + t];
-                    num[i + t] = temp;
+            int gap = t;
+            for (int i = 0; i < num.length; i++) {
+                int temp = num[i];
+                int j = i - gap;
+                for (; j >= 0 && num[j] > temp; j -= gap) {
+                        num[j + gap] = num[j];
                 }
+                num[j + gap] = temp;
             }
         }
 
