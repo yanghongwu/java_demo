4df6da665a02f6cf2e2bd8bc257aa9849a691a18
yanghongwu <yanghongwu103@gmail.com>
2017-06-14 11:53:16 +0800

    udp socket server

--- src/main/java/socket/UDPEchoServer.java
+++ src/main/java/socket/UDPEchoServer.java
@@ -24,6 +24,9 @@ public class UDPEchoServer {
             DatagramPacket packet = new DatagramPacket(new byte[ECHOMAX], ECHOMAX);
 
             while (true) {
+                byte[] resp2 = new byte[]{'1','2','3','4','5','6','1','2','3','4','5','6'};// 数据大于buflength那么多余字节将不会被发送;
+                packet.setData(resp2, 2, 5);   // 只有这个区域会被接收到的字符替换,其余不会变化;
+
                 socket.receive(packet);
                 System.out.println("Handling client at " + packet.getAddress().getHostAddress() + " on port " + packet.getPort());
                 System.out.println("Received " + new String(packet.getData()));
@@ -32,10 +35,11 @@ public class UDPEchoServer {
 //                packet.setData("from server".getBytes());
 
                 byte[] resp = new byte[]{'1','2','3','4','5','6','1','2','3','4','5','6'};// 数据大于buflength那么多余字节将不会被发送;
-                packet.setData(resp);
+                packet.setData(resp, 2, 5);
 
-                packet.setLength(10);  // 实际发送的数据长度,还有buflength表示缓冲区大小
+//                packet.setLength(10);  // 实际发送的数据长度,还有buflength表示缓冲区大小
                 socket.send(packet);
+                System.out.println();
 
             }
         } catch (SocketException e) {
